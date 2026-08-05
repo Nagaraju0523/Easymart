518ef0471c1bc12746a091e82d63232c889632e2
Nagaraju0523 <nagarajug.tammina@gmail.com>
2026-08-06 04:17:27 +0530

    Added customer dashboard and login session

--- easymart/app.py
+++ easymart/app.py
@@ -11,3 +11,5 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+app.secret_key = "easymart123"
--- easymart/routes/customer.py
+++ easymart/routes/customer.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, session, redirect
 from db import get_connection
 
 customer = Blueprint("customer", __name__)
